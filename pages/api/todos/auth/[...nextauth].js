6a136db56dcac74a75202137b1bf1da294790f9a
Adam Kuzniarski <adam.kuzniarski@gmx.de>
2025-09-04 21:18:02 +0300

    add nextaut.js

--- pages/api/todos/auth/[...nextauth].js
+++ pages/api/todos/auth/[...nextauth].js
@@ -1,1 +1,17 @@
-sasd sdad
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+
+  session: { strategy: "jwt" },
+  // Optional: eigene Seite für SignIn, wenn du willst
+  // pages: { signIn: "/signin" },
+};
+
+export default NextAuth(authOptions);
