2a8e844ec6bb656f129874d463e841cb55a0a03f
Adam Kuzniarski <adam.kuzniarski@gmx.de>
2025-09-04 21:21:30 +0300

    add fetcher a. ThemeProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
-import "@/styles/globals.css";
+import GlobalStyle from "@/styles";
+import { SWRConfig } from "swr";
+import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "@/context/ThemeContext";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SWRConfig
+      value={{
+        fetcher: async (...args) => {
+          const response = await fetch(...args);
+          if (!response.ok) throw new Error(`Request with ${JSON.stringify(args)} failed.`);
+          return response.json();
+        },
+      }}
+    >
+      <SessionProvider session={session}>
+        <ThemeProvider>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </SessionProvider>
+    </SWRConfig>
+  );
 }
